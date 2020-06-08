d2f6d080f57e41f09e1c3df5e3f8952be5dfadd9
artermi <ericcheng230@gmail.com>
2020-06-08 20:49:07 +0800

    Modify the game to find two person first mode

--- PGG_game.py
+++ PGG_game.py
@@ -7,8 +7,13 @@ class PGG_5G:
         self.r = r
         self.K = K
         self.L = L
-        self.changed_x = -1
-        self.changed_y = -1
+
+        #x is the chosen player that need to suggest y its strategy
+        self.xi = -1
+        self.xj = -1
+        self.yi = -1
+        self.yj = -1
+
         self.first = True
         self.player_matrix = []
         for i in range(L):
@@ -18,86 +23,90 @@ class PGG_5G:
             self.player_matrix.append(temp_matrix)
 
     def one_play(self,i,j):
-        goods = 1
+        #each we asked the players of grid[i][j] to play
+        goods = self.player_matrix[i][j].allocate()
         goods = goods + self.player_matrix[(i+1) % self.L][j].allocate() + self.player_matrix[(i-1) % self.L][j].allocate() 
         goods = goods + self.player_matrix[i][(j+1) % self.L].allocate() + self.player_matrix[i][(j-1) % self.L].allocate()
 
-        goods = goods * self.r
-        #Now it's time to distribute gains
-        self.player_matrix[i][j].gain_profit(goods/5.0)
-        self.player_matrix[(i+1)%self.L][j].gain_profit(goods/5.0)
-        self.player_matrix[(i-1)%self.L][j].gain_profit(goods/5.0)
-        self.player_matrix[i][(j+1)%self.L].gain_profit(goods/5.0)
-        self.player_matrix[i][(j-1)%self.L].gain_profit(goods/5.0)
+        return goods * self.r / 5.0 #just record the gain
 
 
-    def invest_and_play(self):
-        # after players invest, play right then
-        if self.changed_x > -1 or self.first:
-            for i in range(self.L):
-                for j in range(self.L):
-                    self.player_matrix[i][j].reset()
+    def two_players_play(self):
+        if self.xi > -1: #Means x and y have different strateies
+            L = self.L
+            profit_x = self.one_play(self.xi,self.xj)
+            profit_x = profit_x + self.one_play((self.xi+1)%L,self.xj) + self.one_play((self.xi-1)%L,self.xj)
+            profit_x = profit_x + self.one_play(self.xi,(self.xj+1)%L) + self.one_play(self.xi,(self.xj-1)%L)
+            if self.player_matrix[self.xi][self.xj].isCoop:
+                profit_x = profit_x - 5
 
-            for i in range(self.L):
-                for j in range(self.L):
-                    #Here, everyone invests
-                    self.one_play(i,j)
-            self.first = False
-            self.changed_x = -1
-            return True
-        return False
+            profit_y = self.one_play(self.yi,self.yj)
+            profit_y = profit_y + self.one_play((self.yi+1)%L,self.yj) + self.one_play((self.yi-1)%L,self.yj)
+            profit_y = profit_y + self.one_play(self.yi,(self.yj+1)%L) + self.one_play(self.yi,(self.yj-1)%L)
+            if self.player_matrix[self.yi][self.yj].isCoop:
+                profit_y = profit_y - 5
+            
+            self.player_matrix[self.yi][self.yj].change_strategy(self.player_matrix[self.xi][self.xj],self.K,profit_y,profit_x)
+
+        self.xi = -1
 
 
-    def modify_strategies(self):
+    def choose_players(self):
         i = randint(0,self.L-1)
         j = randint(0,self.L-1)
+
         rand_neigh = choice(['u','d','l','r'])
         if rand_neigh == 'l':
-            if self.player_matrix[(i-1)%self.L][j].change_strategy(self.player_matrix[i][j],self.K):
-                self.changed_x = (i-1)%self.L
-                self.changed_y = j
+            self.yi = (i-1)%self.L
+            self.yj = j
 
         elif rand_neigh == 'r':
-            if self.player_matrix[(i+1)%self.L][j].change_strategy(self.player_matrix[i][j],self.K):
-                self.changed_x = (i+1)%self.L
-                self.changed_y = j
+            self.yi = (i+1)%self.L
+            self.yj = j
         
         elif rand_neigh == 'u':
-            if self.player_matrix[i][(j+1)%self.L].change_strategy(self.player_matrix[i][j],self.K):
-                self.changed_x = i
-                self.changed_y = (j+1)%self.L
+            self.yi = i
+            self.yj = (j+1)%self.L
 
         else:
-            if self.player_matrix[i][(j-1)%self.L].change_strategy(self.player_matrix[i][j],self.K):
-                self.changed_x = i
-                self.changed_y = (j-1)%self.L
+            self.yi = i
+            self.yj = (j-1)%self.L
 
-    def calculate_c(self):
+        #I use self.xi to see if the x,y use the same strategies
+        if self.player_matrix[self.yi][self.yj].isCoop != self.player_matrix[i][j].isCoop:
+            self.xi = i
+            self.xj = j
+        else:
+            self.xi = -1
+
+        return self.xi > -1 #means two players do not use the same strategy
+
+
+    def calculate_rate(self):
         coor = 0
         ttl = self.L * self.L
         for i in range(self.L):
-#            tmp = []
             for j in range(self.L):
-#                tmp.append(1 if self.player_matrix[i][j].isCoop else 0)
                 if self.player_matrix[i][j].isCoop:
                     coor = coor + 1
-#            print(tmp)
         return coor/ttl
 
 
 if __name__ == '__main__':
-    """
+#    """
     game = PGG_5G(5,0.5,40)
 
     for i in range(100000):
-        cal = game.invest_and_play()
-        game.modify_strategies()
-        if not cal:
-            continue
-        per_c = game.calculate_c()
-        print(per_c)
-        if per_c == 0 or per_c == 1:
-            break
+        if i % 500 == 0:
+            per_c = game.calculate_rate()
+            print(i,per_c)
+        for j in range(1600):
+            modi = game.choose_players()
+            if not modi:
+                continue
+        #If the strategy is not modified, no need to play the game
+            game.two_players_play()
+
     
     """
     for j in range(25):
@@ -107,4 +116,4 @@ if __name__ == '__main__':
             game.invest_and_play()
             game.modify_strategies()
         print((0.8+0.02*j),game.calculate_c())
-#    """
+    """
