83229bdadd3f57bcc6cf35b0aa077a969e2c1a1a
artermi <ericcheng230@gmail.com>
2020-06-08 19:29:20 +0800

    externalize the profit and remove the newCoop

--- player.py
+++ player.py
@@ -6,41 +6,32 @@ class Player:
     def __init__(self,strategy):
         # isCoop is initial strategy. True if it's a cooperator.
         # prof is the initial profit
-        self.isCoop = strategy #Should be true or fause
+        self.isCoop = strategy #Should be true or false
         self.newCoop = self.isCoop
-        self.prof = 5.0
-
 
     def allocate(self):
         #allocate its resource
-        self.isCoop = self.newCoop
+        #self.isCoop = self.newCoop
         if self.isCoop:
-            self.prof = self.prof - 1
             return 1.0
         return 0.0
-
-    def gain_profit(self,gain):
-        self.prof = self.prof + gain
-
-    def reset(self):
-        self.prof = 5.0
         
-    def change_strategy(self, neighbour,K): #K is uncertainty
+    def change_strategy(self, neighbour,K, self_prof,neigh_prof): #K is uncertainty
         prob = 0.0
         if self.isCoop == neighbour.isCoop:
-            return False
+            print("WRONG!! they shouldn't be the same!!")
+            exit()
 
-        if (self.prof - neighbour.prof)/K > 10:
+        if (self_prof - neigh_prof)/K > 10:
             return False
-        elif (self.prof - neighbour.prof)/K < -5:
-            self.newCoop = neighbour.isCoop
+        elif (self_prof - neigh_prof)/K < -5:
+            self.isCoop = neighbour.isCoop
             return True
 
         else:
-            prob = 1.0/(1.0 + math.exp((self.prof - neighbour.prof) /K))
+            prob = 1.0/(1.0 + math.exp((self_prof - neigh_prof) /K))
 
         if choices([True,False],[prob,1.0-prob])[0]:
-            self.newCoop = neighbour.isCoop
-#            print(prob,neighbour.isCoop,self.isCoop)
+            self.isCoop = neighbour.isCoop
             return True
         return False
