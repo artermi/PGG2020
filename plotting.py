c486de7e6669f49097ef5f9cebb65f9824827a8e
artermi <ericcheng230@gmail.com>
2020-06-10 01:38:05 +0800

    plotting from files

--- plotting.py
+++ plotting.py
@@ -1,13 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+import statistics
 
-f = open("result.txt")
 rG = []
 lo = []
 
-for line in f:
-     rG.append("%.2f" % float(line.split()[0]) )
-     lo.append(float(line.split()[1]))
+for fi in os.listdir("sim"):
+    num = float(fi[4:8])/5000
+    rG.append(num)
+    with open('sim/' + fi,'r') as f:
+        all_num = []
+        for line in f:
+            rnd, per = int(line.split()[0]), float(line.split()[1])
+            if rnd < 5000:
+                continue
+            all_num.append(per)
+        lo.append(statistics.mean(all_num))
+
 
 plt.plot(rG,lo,'ks')
 plt.show()
+
