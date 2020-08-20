f7b603d5fa9e0af9fff2a43de932fcbee6bbe374
artermi <ericcheng230@gmail.com>
2020-08-20 18:03:58 +0800

    new expFAPGG

--- exp_FAPGG_game.py
+++ exp_FAPGG_game.py
@@ -91,15 +91,17 @@ class FAPGG_5G(PGG_5G):
     """
 
 def do_all_mode():
-    rlist = [2.4,2.41,2.42,2.43,2.44,2.45,2.46,2.47,2.48,2.49,2.5,2.6,2.7,2.8,2.9,
-            3,3.1,3.15,3.17,3.2,3.25,3.3,3.35,3.4,3.5,3.6,
+    rlist = [#2.4,2.41,2.42,2.43,2.44,2.45,2.46,2.47,2.48,2.49,2.5,2.6,2.7,2.8,2.9,
+            #3,3.1,3.15,3.17,3.2,3.25,3.3,3.35,
+            3.4,3.5,3.6,
             3.74,3.747,3.748,3.75,3.76,3.78,3.80,3.82,3.84,3.86,3.88,3.90,
             3.92,3.94,3.96,3.98,4.00,4.05,4.10,4.15,4.20,4.30,4.40,4.50,
             4.60,4.70,4.80,4.90,5.00,5.10,5.20,5.30,5.40,5.44,5.49,5.5]
-    alps = [0.603,1.230,2.672] #0.45 0.4 0.3
+    alps = [1/0.603] #[1/0.603,1/1.230,1/2.672] #0.45 0.4 0.3 // = 1.65, 0.81, 0.374 
+
     paths = []
     for alp in alps:
-        path = 'exp_' + str(int(alp * 100)).zfill(3)
+        path = 'EXP_' + str(int(alp * 100)).zfill(3)
         Path(path).mkdir(parents=True, exist_ok=True)
         paths.append((path,alp))
 
@@ -107,15 +109,21 @@ def do_all_mode():
         p, alp = path
 
         for r in rlist:
-            filename = p + '/alp_' + str(int(alp * 100)).zfill(3) + '_'
+            filename = p + '/lmd_' + str(int(alp * 100)).zfill(3) + '_'
             filename += 'r_' +  str(int(r * 1000) ) + '.dat'
             
             f = open(filename,"w")
             print('Now doing:' + filename)
 
-            game = FAPGG_5G(r,0.5,40,alp) #r,K,L alp
+            L = 200
+            LL = L*L
+
+            game = FAPGG_5G(r,0.5,L,alp) #r,K,L alp
             per_c = 0.5
             for i in range(10001):
+                if i < 1000 and i % 30 == 0:
+                    per_c = game.calculate_rate()
+                    
                 if i % 500 == 0:
                     per_c = game.calculate_rate()
                     f.write(str(i).zfill(6) + ' ' + '%.3f'%(per_c) + '\n')
@@ -124,7 +132,7 @@ def do_all_mode():
                 if per_c == 1 or per_c == 0:
                     continue
 
-                for j in range(1600):
+                for j in range(LL):
                     modi = game.choose_players()
                     if not modi:
                         continue
@@ -138,7 +146,7 @@ def do_alpha_mode():
     paths = []
 
     for r in rs:
-        path = 'expr_' + str(int( r * 1000))
+        path = 'EXP_' + str(int( r * 1000))
         #path = 'r_040'
         Path(path).mkdir(parents=True, exist_ok=True)
         paths.append((path,r))
@@ -154,6 +162,8 @@ def do_alpha_mode():
 
             game = FAPGG_5G(r,0.5,40,alp) #r,K,L alp
             for i in range(10001):
+
+
                 if i % 500 == 0:
                     per_c = game.calculate_rate()
                     f.write(str(i).zfill(6) + ' ' + '%.3f'%(per_c) + '\n')
